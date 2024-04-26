81e6bfd110f23f5cbe33f1a4a39826bc3cb475db
Kevin Gut <kevin.gut@bitm.sg>
2024-04-26 15:27:10 +0200

    Switch to newer library

--- js/ip.js
+++ js/ip.js
@@ -1,33 +1,16 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __classPrivateFieldGet = (this && this.__classPrivateFieldGet) || function (receiver, state, kind, f) {
-    if (kind === "a" && !f) throw new TypeError("Private accessor was defined without a getter");
-    if (typeof state === "function" ? receiver !== state || !f : !state.has(receiver)) throw new TypeError("Cannot read private member from an object whose class did not declare it");
-    return kind === "m" ? f : kind === "a" ? f.call(receiver) : f ? f.value : state.get(receiver);
-};
-var __classPrivateFieldSet = (this && this.__classPrivateFieldSet) || function (receiver, state, value, kind, f) {
-    if (kind === "m") throw new TypeError("Private method is not writable");
-    if (kind === "a" && !f) throw new TypeError("Private accessor was defined without a setter");
-    if (typeof state === "function" ? receiver !== state || !f : !state.has(receiver)) throw new TypeError("Cannot write private member to an object whose class did not declare it");
-    return (kind === "a" ? f.call(receiver, value) : f ? f.value = value : state.set(receiver, value)), value;
-};
 var AyrA;
 (function (AyrA) {
-    var _IpError_errorCode, _IpError_extraData;
     /** Base error class for all manually thrown exceptions. This inherits from js Error class */
     class IpError extends Error {
+        /** Error code */
+        #errorCode;
+        /** Optional extra data */
+        #extraData;
         /** Gets the error code associated with this error */
-        get code() { return __classPrivateFieldGet(this, _IpError_errorCode, "f"); }
+        get code() { return this.#errorCode; }
         /** Gets additional data supplied to this error */
-        get data() { return __classPrivateFieldGet(this, _IpError_extraData, "f"); }
+        get data() { return this.#extraData; }
         /**
          * Creates an IpError instance
          * @param message Create new instance
@@ -36,15 +19,10 @@ var AyrA;
          */
         constructor(message, code, extraData) {
             super(message);
-            /** Error code */
-            _IpError_errorCode.set(this, void 0);
-            /** Optional extra data */
-            _IpError_extraData.set(this, void 0);
-            __classPrivateFieldSet(this, _IpError_errorCode, code, "f");
-            __classPrivateFieldSet(this, _IpError_extraData, extraData, "f");
+            this.#errorCode = code;
+            this.#extraData = extraData;
         }
     }
-    _IpError_errorCode = new WeakMap(), _IpError_extraData = new WeakMap();
     AyrA.IpError = IpError;
     /**
      * Possible error codes for the code property of IpError
@@ -63,16 +41,14 @@ var AyrA;
      * Gets client IPv4 and v6 address
      * @returns Object literal containing client IP addresses. Fields may be null if the address is unobtainable
      */
-    function getIpAddresses() {
-        return __awaiter(this, void 0, void 0, function* () {
-            //Do requests in parallel
-            const v4 = getIpv4(true);
-            const v6 = getIpv6(true);
-            return {
-                v4: yield v4,
-                v6: yield v6
-            };
-        });
+    async function getIpAddresses() {
+        //Do requests in parallel
+        const v4 = getIpv4(true);
+        const v6 = getIpv6(true);
+        return {
+            v4: await v4,
+            v6: await v6
+        };
     }
     AyrA.getIpAddresses = getIpAddresses;
     /**
@@ -80,10 +56,8 @@ var AyrA;
      * @param errorAsNull If true, return null instead of throwing errors
      * @returns IP address
      */
-    function getIp(errorAsNull) {
-        return __awaiter(this, void 0, void 0, function* () {
-            return yield req("ip", errorAsNull);
-        });
+    async function getIp(errorAsNull) {
+        return await req("ip", errorAsNull);
     }
     AyrA.getIp = getIp;
     /**
@@ -91,10 +65,8 @@ var AyrA;
      * @param errorAsNull If true, return null instead of throwing errors
      * @returns IPv4 address
      */
-    function getIpv4(errorAsNull) {
-        return __awaiter(this, void 0, void 0, function* () {
-            return yield req("ip4", errorAsNull);
-        });
+    async function getIpv4(errorAsNull) {
+        return await req("ip4", errorAsNull);
     }
     AyrA.getIpv4 = getIpv4;
     /**
@@ -102,10 +74,8 @@ var AyrA;
      * @param errorAsNull If true, return null instead of throwing errors
      * @returns IPv6 address
      */
-    function getIpv6(errorAsNull) {
-        return __awaiter(this, void 0, void 0, function* () {
-            return yield req("ip6", errorAsNull);
-        });
+    async function getIpv6(errorAsNull) {
+        return await req("ip6", errorAsNull);
     }
     AyrA.getIpv6 = getIpv6;
     /**
@@ -114,32 +84,30 @@ var AyrA;
      * @param errorAsNull true, to return null on errors, false to throw an exception
      * @returns IP address, or null on failure if errorAsNull is set
      */
-    function req(sub, errorAsNull) {
-        return __awaiter(this, void 0, void 0, function* () {
-            if (!isSubdomain(sub)) {
-                if (errorAsNull) {
-                    return null;
-                }
-                throw new Error(`Invalid subdomain: ${sub}`);
+    async function req(sub, errorAsNull) {
+        if (!isSubdomain(sub)) {
+            if (errorAsNull) {
+                return null;
             }
-            try {
-                const response = yield fetch(`https://${sub}.ayra.ch/ip`);
-                if (!response.ok) {
-                    throw new IpError("Got unexpected status response", ErrorCodes.InvalidHttpStatus, { status: response.status, statusText: response.statusText });
-                }
-                const body = (yield response.text()).trim();
-                if (!isIp(body)) {
-                    throw new IpError("Server returned a value that is not an IP address", ErrorCodes.InvalidAddress, body);
-                }
-                return body;
+            throw new Error(`Invalid subdomain: ${sub}`);
+        }
+        try {
+            const response = await fetch(`https://${sub}.ayra.ch/ip`);
+            if (!response.ok) {
+                throw new IpError("Got unexpected status response", ErrorCodes.InvalidHttpStatus, { status: response.status, statusText: response.statusText });
             }
-            catch (e) {
-                if (errorAsNull) {
-                    return null;
-                }
-                throw e;
+            const body = (await response.text()).trim();
+            if (!isIp(body)) {
+                throw new IpError("Server returned a value that is not an IP address", ErrorCodes.InvalidAddress, body);
             }
-        });
+            return body;
+        }
+        catch (e) {
+            if (errorAsNull) {
+                return null;
+            }
+            throw e;
+        }
     }
     /**
      * Checks if the value is a valid IPv4 or IPv6 address
